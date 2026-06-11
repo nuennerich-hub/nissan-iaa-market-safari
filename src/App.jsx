a12977fb6e5e547f49f12e32e70e44d61710c756
nuennerich-hub <nuennerich@condumani.de>
2026-06-11 10:33:26 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -365,7 +365,7 @@ export default function App() {
 
   return (
     <div className="min-h-screen bg-[#f5f5f5] text-[#111]">
-      <div className="max-w-7xl mx-auto p-4 md:p-8 space-y-6">
+      <div className="max-w-[1600px] mx-auto p-6 md:p-10 space-y-8">
         <Header />
 
         {statusMessage && (
@@ -430,7 +430,7 @@ function Header() {
           <img
             src="/new-Nissan-logo-black-png-large-size.png"
             alt="Nissan Logo"
-            className="h-16 md:h-24 w-auto object-contain"
+            className="h-12 md:h-16 w-auto object-contain"
           />
 
           <div>
